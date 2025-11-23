39f8240d29a0498a4227733cba009bcc968496ff
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-11-23 01:47:25 +0100

    Update fetch_price_data.py

--- fetch_price_data.py
+++ fetch_price_data.py
@@ -7,8 +7,8 @@ def fetch_btc_candles():
     base_url = 'https://api.binance.com/api/v3/klines'
     symbol = 'BTCUSDT'
     interval = '1d'
-    start_date = datetime(2024, 1, 1)
-    end_date = datetime(2025, 11, 30)
+    start_date = datetime(2022, 1, 1)
+    end_date = datetime(2025, 12, 31)
     
     all_data = []
     current_start = start_date
