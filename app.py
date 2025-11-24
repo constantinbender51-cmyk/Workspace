84664b7077c286e0b20663279fa35c5b2546c988
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-11-24 09:09:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -323,7 +323,7 @@ def run_training_task():
         X_test_reshaped = X_test.reshape(X_test.shape[0], 20, 10)
         
         # INCREASED EPOCHS AND ADDED REGULARIZATION
-        EPOCHS = 3
+        EPOCHS = 100
         UNITS = 20
         REG_RATE = 1e-2 # L2 Regularization rate
         
